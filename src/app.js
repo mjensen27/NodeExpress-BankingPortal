ca3f5b9028717cf50460b58f8cf650d4a031a58c
Matthew Kirk Jensen <matthew.k.jensen@gmail.com>
2019-11-01 15:21:42 -0400

    Completed Module 3

--- src/app.js
+++ src/app.js
@@ -9,8 +9,30 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-fs.readFileSync(/json/users.json, {encoding: 'UTF8'})
+const accountData = fs.readFileSync("src/json/accounts.json", {
+  encoding: "UTF8"
+});
+const accounts = JSON.parse(accountData);
 
-app.get("/", (req, res) => res.render("index", { title: "Index" }));
+const userData = fs.readFileSync("src/json/users.json", { encoding: "UTF8" });
+const users = JSON.parse(userData);
+
+app.get("/", (req, res) =>
+  res.render("index", { title: "Account Summary", accounts: accounts })
+);
+
+app.get("/savings", (req, res) =>
+  res.render("account", { account: accounts.savings })
+);
+
+app.get("/checking", (req, res) =>
+  res.render("account", { account: accounts.checking })
+);
+
+app.get("/credit", (req, res) =>
+  res.render("account", { account: accounts.credit })
+);
+
+app.get("/profile", (req, res) => res.render("profile", { user: users[0] }));
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
