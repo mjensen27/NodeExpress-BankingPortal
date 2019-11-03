4d5384bf640aee315044052e4a87da83c2d13526
Matthew Jensen <matthew.k.jensen@gmail.com>
2019-11-03 14:56:38 -0500

    Completed module

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({ extended: true }));
+
 const accountData = fs.readFileSync("src/json/accounts.json", {
   encoding: "UTF8"
 });
@@ -34,5 +36,20 @@ app.get("/credit", (req, res) =>
 );
 
 app.get("/profile", (req, res) => res.render("profile", { user: users[0] }));
+
+app.get("/transfer", (req, res) => res.render("transfer"));
+app.post("/transfer", (req, res) => {
+  accounts[req.body.from].balance =
+    accounts[req.body.from].balance - req.body.amount;
+  accounts[req.body.to].balance =
+    accounts[req.body.to].balance + parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(
+    path.join(__dirname, "json/accounts.json"),
+    accountsJSON,
+    "utf8"
+  );
+  res.render("transfer", { message: "Transfer Completed" });
+});
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
